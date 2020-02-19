4f1a4829ecc5d7e48e64905ad7aad8ce94290d3a
Craig Peacock <craigp@beyondlogic.org>
2020-02-19 03:34:25 -0800

    Basic HelloWorld template

---
+++ main/esp32_can.c
@@ -0,0 +1,20 @@
+
+#include <stdio.h>
+#include "sdkconfig.h"
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "esp_system.h"
+#include "esp_spi_flash.h"
+
+#ifdef CONFIG_IDF_TARGET_ESP32
+#define CHIP_NAME "ESP32"
+#endif
+
+#ifdef CONFIG_IDF_TARGET_ESP32S2BETA
+#define CHIP_NAME "ESP32-S2 Beta"
+#endif
+
+void app_main(void)
+{
+    printf("Hello world!\n");
+}
